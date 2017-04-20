32261527da3e9f7e9b84cf51156cfa7cc229f662
Balaji Ramalingam <b.ramalingam@dundee.ac.uk>
2017-04-20 12:28:24 +0100

    fix docker running script to include mounting of Matlab and also run a shell script to install matlab python engine?

--- jupyter.sh
+++ jupyter.sh
@@ -1,2 +1,3 @@
-# Create ./notebooks with chmod o+wt 
-exec docker run --name ome-jupyter -d -p 8888:8888 -v "/data/notebooks:/notebooks" ome-jupyter
+# Create ./notebooks with chmod o+wt
+exec docker run --name notebook_latest -d -p 8888:8888 -v /usr/local/MATLAB/R2017a:/usr/local/MATLAB/from-host --mac-address=00:00:00:00:00:00 -v "/home/parallels/Desktop/jupyter-docker/data:/notebooks" notebook_latest
+#exec docker exec notebook_latest /InstallMatlabPythonEngine.sh
