c4aa2ce5b0f82afdbb17809b4307cac0d71f5fbf
Balaji Ramalingam <b.ramalingam@dundee.ac.uk>
2017-05-05 13:03:59 +0100

    Add createImagefromNumpy for omero.figure script, and also add saving feature for Matplotlib figure

--- SharpnessScript.py
+++ SharpnessScript.py
@@ -5,12 +5,18 @@ import matplotlib.pyplot as plt
 import decimal
 import seaborn as sns
 from mpl_toolkits.axes_grid1 import AxesGrid
+from numpy import array, int8
 
 from omero.gateway import BlitzGateway
 # from Parse_OMERO_Properties import USERNAME, PASSWORD, HOST, PORT
 # from Parse_OMERO_Properties import datasetId, imageId, plateId
 
 if __name__ == '__main__':
+
+    def planeGen():
+        """generator will yield planes"""
+        for p in planes:
+            yield p
     
     USERNAME = ""
     PASSWORD = ""
@@ -20,71 +26,59 @@ if __name__ == '__main__':
 
     conn = BlitzGateway(USERNAME, PASSWORD, host=HOST, port=PORT)
     conn.connect()
-    # Using secure connection.
-    # By default, once we have logged in, data transfer is not encrypted
-    # (faster)
-    # To use a secure connection, call setSecure(True):
     conn.setSecure(True);
 
     plate = conn.getObject("Plate", plateId)
     print "\nNumber of fields:", plate.getNumberOfFields()
     print "\nGrid size:", plate.getGridSize()
     print "\nWells in Plate:", plate.getName()
+
+    plate_name = plate.getName()
+    size_z = plate.getNumberOfFields()
+    size_t = 1
     cntr=0
     for well in plate.listChildren():
         index = well.countWellSample()
-        print "  Well: ", well.row, well.column, " Fields:", index
+        print "  Well: ", well.row, well.column, " Fields:", index        
+        #Hack for writing it into an image, will be used for making omero.figures (split-view) in an automated manner 
+        size_z = index
+
         for index in xrange(0, index):
                 pixels = well.getImage(index).getPrimaryPixels()
-                channels = well.getImage(index).getSizeC();
+                size_c = well.getImage(index).getSizeC();
                 if cntr==0:
-                    result_array = np.zeros((96,4*channels), dtype=int);
-                for ch in xrange(0, channels):
+                    result_array = np.zeros((96,4*size_c), dtype=int);
+                for ch in xrange(0, size_c):
                     plane = pixels.getPlane(0, ch, 0)
                     gy, gx = np.gradient(plane)
                     gnorm = np.sqrt(gx**2 + gy**2)
                     sharpness = np.average(gnorm)
-                    print "\n,Sharpness Score:", sharpness
-                    print ((well.row)*12)+well.column
                     result_array[((well.row)*12)+well.column,(index+ch*4)]=sharpness;
                     cntr=cntr+1;
     print result_array
-   
+  
     # #Binary data
-    np.save('maximums.npy', result_array)
+    # np.save('maximums.npy', result_array)
     result_array = np.load('maximums.npy')
     vals=[]
+    planes=[]
     for colval in range (0, result_array.shape[1]):
         data = result_array[:,colval].reshape(8,12);
+        data = np.repeat(data, 20, axis=1)
+        data = np.repeat(data, 20, axis=0)
+        planes.append(np.uint16(data))
         ax = plt.subplot(4,4,colval+1)
         plt.pcolor(data)
         plt.axis('off')
         plt.colorbar()
         ax.title.set_text(colval)
+    savefig(plate_name + 'SharpnessHeatMaps.png')
     plt.show()
+    # print planes
 
-    # Secondary Representations
-     # plt.imshow(result_array, cmap=matplotlib.cm.Reds, vmin=np.min(result_array[np.nonzero(result_array)])-100, vmax=np.max(result_array[np.nonzero(result_array)])+100)
-
-
-    # # get the tick label font size
-    # fontsize_pt = plt.rcParams['ytick.labelsize']
-    # dpi = 72.27
-    # # compute the matrix height in points and inches
-    # matrix_height_pt = fontsize_pt * data.shape[0]
-    # matrix_height_in = matrix_height_pt / dpi
-
-    # # compute the required figure height 
-    # top_margin = 0.04  # in percentage of the figure height
-    # bottom_margin = 0.04 # in percentage of the figure height
-    # figure_height = matrix_height_in / (1 - top_margin - bottom_margin)
-
-    # # build the figure instance with the desired height
-    # fig, ax = plt.subplots(
-    #         figsize=(6,figure_height), 
-    #         gridspec_kw=dict(top=1-top_margin, bottom=bottom_margin))
-
-    # # let seaborn do it's thing
-    # ax = sns.heatmap(data, ax=ax)
-    # # save the figure
-    # plt.savefig('test.png')
+    desc = "Image created from a hard-coded arrays"
+    i = conn.createImageFromNumpySeq(
+    planeGen(), plate_name + "numpy image", size_z, size_c, size_t, description=desc,
+    dataset=None)
+    print 'Created new Image:%s Name:"%s"' % (i.getId(), i.getName())
+    conn.close()
